b9ea8aa81dbb91ed96f6f50d0bd69fa5cc7712da
John Gleason <jgleason@sajan.com>
2016-03-22 13:49:04 -0500

    using spread operator in the reducer instead of object.assign

--- src/reducers/authentication.js
+++ src/reducers/authentication.js
@@ -1,43 +1,48 @@
 import * as AuthConstants from '../constants/authentication';
 
 const initialState = {
-  isLoggingIn: false
+    isLoggingIn: false
 };
 
 const authentication = (state = initialState, action) => {
     switch (action.type) {
         case AuthConstants.LOG_IN_REQUEST:
         {
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 isLoggingIn: true
-            });
+            };
         }
 
         case AuthConstants.LOG_IN_SUCCESS:
         {
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 isLoggingIn: false,
                 user: action.payload.user
-            });
+            };
         }
 
         case AuthConstants.LOG_IN_ERROR:
         {
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 isLoggingIn: false,
                 error: action.error
-            });
+            };
         }
 
         case AuthConstants.LOG_OUT:
         {
-            return Object.assign({}, state, {
+            return {
+                ...state,
                 isLoggingIn: false,
                 user: null
-            });
+            };
         }
 
-        default: {
+        default:
+        {
             return state;
         }
     }
